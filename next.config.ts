019a8bbf20199573def23da463d870a101b8817f
webLion200 <renzhiwei2014@outlook.com>
2025-01-13 07:57:15 -0500

    singup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@node-rs/argon2"],
 };
 
 export default nextConfig;
---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,75 @@
+import { lucia } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { SignUpValues, signUpSchema } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function singUp(credentials: SignUpValues): Promise<{error:string}> {
+    try {
+        const {username, password, email} = signUpSchema.parse(credentials)
+
+        const passwordHash = await hash(password, {
+            // recommended minimum parameters
+            memoryCost: 19456,
+            timeCost: 2,
+            outputLen: 32,
+            parallelism: 1
+        });
+        const userId = generateIdFromEntropySize(10);
+        
+        const existingUsername = await prisma.user.findFirst({
+            where: {
+                username: {
+                    equals: username,
+                    mode: 'insensitive',
+                },
+            }
+        })
+        if(existingUsername) {
+            return {
+                error: "Username already taken"
+            }
+        }
+
+        const existingEmail = await prisma.user.findFirst({
+            where: {
+                email: {
+                    equals: email,
+                    mode: 'insensitive',
+                },
+            }
+        })
+        if (existingEmail) {
+            return {
+              error: "Email already taken",
+            };
+          }
+
+
+        await prisma.user.create({
+            data: {
+                id: userId,
+                username,
+                displayName: username,
+                email,
+                passwordHash
+            }
+        });
+
+        const session = await lucia.createSession(userId, {});
+	const sessionCookie = lucia.createSessionCookie(session.id);
+	(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+	return redirect("/");
+        
+
+    } catch(error) {
+        if (isRedirectError(error)) throw error;
+    console.error(error);
+        return {
+            error: "Something went wrong. Please try again.",
+          };
+    }
+}
