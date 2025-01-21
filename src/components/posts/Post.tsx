a67d33db65b0cb5e2e289876770a050961cd2e22
webLion200 <renzhiwei2014@outlook.com>
2025-01-21 07:47:19 -0500

    posts list

---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,32 @@
+import { PostData } from "@/lib/types";
+import Link from "next/link";
+import UserAvatar from "../UserAvatar";
+import { formatRelativeDate } from "@/lib/utils";
+
+interface PostProps {
+  post: PostData;
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex flex-wrap gap-3">
+        <Link href={`/users/${post.user.username}`}>
+          <UserAvatar avatarUrl={post.user.avatarUrl} />
+        </Link>
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link href={`/posts/${post.id}`}>
+            <span>{formatRelativeDate(post.createdAt)}</span>
+          </Link>
+        </div>
+        <div className="whitespace-pre-line break-words">{post.content}</div>
+      </div>
+    </article>
+  );
+}
