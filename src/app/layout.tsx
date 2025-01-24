e1e060bfae9168ed36b35caa283022913d10757a
webLion200 <renzhiwei2014@outlook.com>
2025-01-24 02:48:12 -0500

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import ReactQueryProvider from "./ReactQueryProvider";
+import ReactQueryProvider from "../components/ReactQueryProvider";
 import "./globals.css";
 
 const geistSans = Geist({
